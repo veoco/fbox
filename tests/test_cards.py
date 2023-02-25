d97e3f7ac3397a0c2c1e6493ccc998c5447d26d6
veoco <one@nomox.cn>
2023-02-25 15:27:08 +0800

    新增 card 更新测试

--- tests/test_cards.py
+++ tests/test_cards.py
@@ -8,7 +8,31 @@ def test_create_card(client: Client):
     assert r.status_code == 201
 
     res = r.json()
-    token = res['token']
+    token = res["token"]
 
-    r = client.get(f"{BASE_URL}/api/cards/detail", headers={"Authorization": f"Bearer {token}"})
+    r = client.get(
+        f"{BASE_URL}/api/cards/detail", headers={"Authorization": f"Bearer {token}"}
+    )
     assert r.status_code == 200
+
+
+def test_renew_card(client: Client):
+    r = client.post(f"{BASE_URL}/api/cards/", headers={"Token": ADMIN_PASSWORD})
+    res = r.json()
+    token = res["token"]
+
+    r = client.get(
+        f"{BASE_URL}/api/cards/renew", headers={"Authorization": f"Bearer {token}"}
+    )
+    res = r.json()
+    new_token = res["token"]
+
+    r = client.get(
+        f"{BASE_URL}/api/cards/detail", headers={"Authorization": f"Bearer {token}"}
+    )
+    assert r.status_code == 404
+
+    r = client.get(
+        f"{BASE_URL}/api/cards/detail", headers={"Authorization": f"Bearer {new_token}"}
+    )
+    assert r.status_code == 404
