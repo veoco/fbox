5910e2f5030a109deb047a5e205c066a97063697
veoco <one@nomox.cn>
2023-02-25 15:10:52 +0800

    新增 card_detail 相关测试

--- tests/test_cards.py
+++ tests/test_cards.py
@@ -6,3 +6,9 @@ from tests.common import BASE_URL, ADMIN_PASSWORD
 def test_create_card(client: Client):
     r = client.post(f"{BASE_URL}/api/cards/", headers={"Token": ADMIN_PASSWORD})
     assert r.status_code == 201
+
+    res = r.json()
+    token = res['token']
+
+    r = client.get(f"{BASE_URL}/api/cards/detail", headers={"Authorization": f"Bearer {token}"})
+    assert r.status_code == 200
