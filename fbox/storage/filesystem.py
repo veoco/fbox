7914b9470bda7121f6eba95f241e7137f317576a
veoco <one@nomox.cn>
2023-02-28 10:33:46 +0800

    调整代码结构

--- fbox/storage/filesystem.py
+++ fbox/storage/filesystem.py
@@ -30,70 +30,21 @@ class FileSystemStorage(LocalStorage):
     async def close(self) -> None:
         pass
 
-    async def get_filepath(self, code: str, filename: str) -> PurePath:
-        return PurePath("box") / code / "files" / filename
-
-    def _save_dummy(self, filepath: PurePath, size: int):
-        path = settings.DATA_ROOT / filepath
-        if not path.parent.exists():
-            path.parent.mkdir(parents=True)
-        with open(path, "wb") as f:
-            f.truncate(size)
-
     async def save_dummy_file(self, code: str, filename: str, size: int) -> str:
         filepath = await self.get_filepath(code, filename)
         await asyncio.to_thread(self._save_dummy, filepath, size)
         return f"/api/files/{code}/{filename}"
 
-    def _save_slice(self, filepath: PurePath, file: BinaryIO, offset: int):
-        path = settings.DATA_ROOT / filepath
-        with open(path, "r+b") as f:
-            f.seek(offset)
-            chunk = file.read(self.CHUNK_SIZE)
-            while chunk:
-                f.write(chunk)
-                chunk = file.read(self.CHUNK_SIZE)
-
-    async def save_file_slice(
-        self, code: str, filename: str, file: UploadFile, offset: int
-    ) -> None:
-        filepath = await self.get_filepath(code, filename)
-        await asyncio.to_thread(self._save_slice, filepath, file.file, offset)
-
     async def complete_file(
         self, code: str, filename: str, sha256: str, extra: dict
     ) -> bool:
-        file_sha256 = await self.get_file_sha256(code, filename)
+        file_sha256 = await self._get_file_sha256(code, filename)
         if file_sha256 == sha256:
             return True
         return False
 
     async def get_url(self, code: str, filename: str) -> str:
         return f"/api/files/{code}/{filename}"
-
-    def _sha256(self, file: BinaryIO):
-        m = hashlib.sha256()
-        chunk = file.read(self.CHUNK_SIZE)
-        while chunk:
-            m.update(chunk)
-            chunk = file.read(self.CHUNK_SIZE)
-        file.seek(0, os.SEEK_SET)
-        return m.hexdigest()
-
-    async def get_sha256(self, file: BinaryIO) -> str:
-        return await asyncio.to_thread(self._sha256, file)
-
-    async def get_file_sha256(self, code: str, filename: str) -> str:
-        path = settings.DATA_ROOT / await self.get_filepath(code, filename)
-        with open(path, "rb") as f:
-            return await asyncio.to_thread(self._sha256, f)
-
-    async def get_size(self, file: UploadFile) -> int:
-        f = file.file
-        f.seek(0, os.SEEK_END)
-        size = f.tell()
-        f.seek(0, os.SEEK_SET)
-        return size
 
     async def get_capacity(self) -> int:
         total, used, free = await asyncio.to_thread(disk_usage, settings.DATA_ROOT)
@@ -107,6 +58,73 @@ class FileSystemStorage(LocalStorage):
             filenames.append(filename)
         return filenames
 
+    async def get_box(self, code: str) -> Box | None:
+        return await asyncio.to_thread(self._get_box, code)
+
+    async def save_box(self, box: Box) -> None:
+        await asyncio.to_thread(self._save_box, box)
+
+    async def remove_box(self, code: str) -> None:
+        await asyncio.to_thread(self._remove_box, code)
+
+    async def archive_box(self, box: Box) -> None:
+        await asyncio.to_thread(self._archive_box, box)
+
+    async def get_card(self, code: str) -> Card | None:
+        return await asyncio.to_thread(self._get_card, code)
+
+    async def save_card(self, card: Card) -> None:
+        await asyncio.to_thread(self._save_card, card)
+
+    async def get_filepath(self, code: str, filename: str) -> PurePath:
+        return PurePath("box") / code / "files" / filename
+
+    async def save_file_slice(
+        self, code: str, filename: str, file: UploadFile, offset: int
+    ) -> None:
+        filepath = await self.get_filepath(code, filename)
+        await asyncio.to_thread(self._save_slice, filepath, file.file, offset)
+
+    async def get_sha256(self, file: BinaryIO) -> str:
+        return await asyncio.to_thread(self._sha256, file)
+
+    async def get_size(self, file: UploadFile) -> int:
+        f = file.file
+        f.seek(0, os.SEEK_END)
+        size = f.tell()
+        f.seek(0, os.SEEK_SET)
+        return size
+
+    async def _get_file_sha256(self, code: str, filename: str) -> str:
+        path = settings.DATA_ROOT / await self.get_filepath(code, filename)
+        with open(path, "rb") as f:
+            return await asyncio.to_thread(self._sha256, f)
+
+    def _save_dummy(self, filepath: PurePath, size: int):
+        path = settings.DATA_ROOT / filepath
+        if not path.parent.exists():
+            path.parent.mkdir(parents=True)
+        with open(path, "wb") as f:
+            f.truncate(size)
+
+    def _save_slice(self, filepath: PurePath, file: BinaryIO, offset: int):
+        path = settings.DATA_ROOT / filepath
+        with open(path, "r+b") as f:
+            f.seek(offset)
+            chunk = file.read(self.CHUNK_SIZE)
+            while chunk:
+                f.write(chunk)
+                chunk = file.read(self.CHUNK_SIZE)
+
+    def _sha256(self, file: BinaryIO):
+        m = hashlib.sha256()
+        chunk = file.read(self.CHUNK_SIZE)
+        while chunk:
+            m.update(chunk)
+            chunk = file.read(self.CHUNK_SIZE)
+        file.seek(0, os.SEEK_SET)
+        return m.hexdigest()
+
     def _get_box(self, code: str) -> Box | None:
         box_json = settings.DATA_ROOT / "box" / code / "box.json"
         if box_json.exists():
@@ -114,23 +132,14 @@ class FileSystemStorage(LocalStorage):
             return box
         return None
 
-    async def get_box(self, code: str) -> Box | None:
-        return await asyncio.to_thread(self._get_box, code)
-
     def _save_box(self, box: Box) -> None:
         box_file = settings.DATA_ROOT / "box" / box.code / "box.json"
         with open(box_file, "w") as f:
             f.write(box.json())
 
-    async def save_box(self, box: Box) -> None:
-        await asyncio.to_thread(self._save_box, box)
-
     def _remove_box(self, code: str) -> None:
         box_dir = settings.DATA_ROOT / "box" / code
         shutil.rmtree(box_dir)
-
-    async def remove_box(self, code: str) -> None:
-        await asyncio.to_thread(self._remove_box, code)
 
     def _archive_box(self, box: Box) -> None:
         now = get_now().date().isoformat()
@@ -142,9 +151,6 @@ class FileSystemStorage(LocalStorage):
             shutil.move(f, target)
         current.rmdir()
 
-    async def archive_box(self, box: Box) -> None:
-        await asyncio.to_thread(self._archive_box, box)
-
     def _get_card(self, code: str) -> Card | None:
         card_json = settings.DATA_ROOT / "card" / f"{code}.json"
         if card_json.exists():
@@ -152,13 +158,7 @@ class FileSystemStorage(LocalStorage):
             return card
         return None
 
-    async def get_card(self, code: str) -> Card | None:
-        return await asyncio.to_thread(self._get_card, code)
-
     def _save_card(self, card: Card) -> None:
         card_json = settings.DATA_ROOT / "card" / f"{card.code}.json"
         with open(card_json, "w") as f:
             f.write(card.json())
-
-    async def save_card(self, card: Card) -> None:
-        await asyncio.to_thread(self._save_card, card)
